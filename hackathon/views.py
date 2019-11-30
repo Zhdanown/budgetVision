57f41a8482c0ebc37563a696309455dcc6b4bd39
davids <davidskursk@gmail.ccom>
2019-11-30 17:23:14 +0300

    add abiut page

--- hackathon/views.py
+++ hackathon/views.py
@@ -8,13 +8,24 @@ from django.shortcuts import get_object_or_404
 from django.db.models import Q, Max, Count
 from django.db.models import Q, Max, Count
 
-# Create your views here.
+
 def home(request):
     assert isinstance(request, HttpRequest)
 
     return render(
         request,
         'index.html',
+        {            
+            'year': datetime.now().year,    
+        }
+    )
+
+def about(request):
+    assert isinstance(request, HttpRequest)
+
+    return render(
+        request,
+        'about.html',
         {            
             'year': datetime.now().year,    
         }
